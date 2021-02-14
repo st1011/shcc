6e9e5b4052ab1eee20d966f4470638b1a29b0e8a
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2021-02-14 13:20:42 +0900

    テストに失敗したときもinputを表示するように

--- test/test.sh
+++ test/test.sh
@@ -23,7 +23,8 @@ try() {
 	if [ "$actual" = "$expected" ]; then
 		echo "$input => $actual"
 	else
-		echo "$expected expected, but got $actual (L$BASH_LINENO)"
+		echo "*** '$input'"
+		echo "*** $expected expected, but got $actual (L$BASH_LINENO)"
 		exit 1
 	fi
 }
@@ -127,6 +128,7 @@ try 85 'foo(a b c){return a+b+c;} main(){a=20; b=40; c=10; d=foo(a*2 b c/2);retu
 try 42 'main(){{} {;} ; return 42;}'
 
 try 5 'main(){a=-5; b=+10; return a+b;}'
+
 try 42 'main(){if(1) return 42; return 0;}'
 try 42 'main(){if(0) return 0; return 42;}'
 try 42 'main(){if(0) return 0; else return 42; return 1;}'
